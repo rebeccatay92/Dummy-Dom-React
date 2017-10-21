85abf85e4bb2cbe92cb95a4e8e9c7a6b5088dcd5
yangtheng <yangtheng92@gmail.com>
2017-10-21 18:41:00 +0800

    removed comments

--- src/components/Planner.js
+++ src/components/Planner.js
@@ -1,26 +1,5 @@
 import React, { Component } from 'react'
 import DateBox from './Date'
-// import PlannerActivity from './PlannerActivity'
-// import { DropTarget } from 'react-dnd'
-// import { connect } from 'react-redux'
-// import { addActivity, deleteActivity, hoverOutsidePlanner } from '../actions/plannerActions'
-// import { addActivityToBucket, deleteActivityFromBucket } from '../actions/bucketActions'
-
-// const plannerTarget = {
-//   drop (props, monitor) {
-//     if (props.activities.length === 0) {
-//       props.addActivity(monitor.getItem())
-//       props.deleteActivityFromBucket(monitor.getItem())
-//     }
-//   }
-// }
-//
-// function collect (connect, monitor) {
-//   return {
-//     connectDropTarget: connect.dropTarget(),
-//     isOver: monitor.isOver()
-//   }
-// }
 
 class Planner extends Component {
   constructor (props) {
@@ -31,20 +10,7 @@ class Planner extends Component {
     }
   }
 
-  // handleClick (activity) {
-  //   this.props.deleteActivity(activity)
-  //   this.props.addActivityToBucket(activity)
-  // }
-
   render () {
-    // const { connectDropTarget, isOver } = this.props
-    // let preview
-    // if (isOver && this.props.activities.length === 0) {
-    //   preview = (
-    //     <div style={{border: '1px dashed green', height: '12vh'}} />
-    //   )
-    // }
-    // Seeding Dates
     const startDate = new Date(2017, 10, 19)
     const endDate = new Date(2017, 10, 21)
     const getDates = (startDate, stopDate) => {
@@ -60,7 +26,6 @@ class Planner extends Component {
     const newDates = dates.map((date) => {
       return date.toDateString()
     })
-    // console.log(newDates)
     return (
       <div>
         <button onClick={() => this.setState({draggable: !this.state.draggable})}>{this.state.draggable ? 'Rearrange Mode: On' : 'Rearrange Mode: Off'}</button>
@@ -72,38 +37,6 @@ class Planner extends Component {
       </div>
     )
   }
-
-  // componentWillReceiveProps (nextProps) {
-  //   if (nextProps.isOver === !this.props.isOver) {
-  //     if (!nextProps.isOver) this.props.hoverOutsidePlanner()
-  //   }
-  // }
 }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     activities: state.plannerActivities
-//   }
-// }
-//
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     addActivity: (activity) => {
-//       dispatch(addActivity(activity))
-//     },
-//     deleteActivity: (activity) => {
-//       dispatch(deleteActivity(activity))
-//     },
-//     addActivityToBucket: (activity) => {
-//       dispatch(addActivityToBucket(activity))
-//     },
-//     deleteActivityFromBucket: (activity) => {
-//       dispatch(deleteActivityFromBucket(activity))
-//     },
-//     hoverOutsidePlanner: () => {
-//       dispatch(hoverOutsidePlanner())
-//     }
-//   }
-// }
 
 export default Planner
--- src/reducers/plannerReducer.js
+++ src/reducers/plannerReducer.js
@@ -9,14 +9,6 @@ export const plannerReducer = (state = [], action) => {
           action.activity
         ]
       } else {
-        // let newState = state.filter(activity => {
-        //   return activity.id
-        // })
-        // return [
-        //   ...newState.slice(0, action.index),
-        //   ...[action.activity],
-        //   ...newState.slice(action.index, state.length)
-        // ]
         let stateWithoutActivitiesWithThatDate = state.filter(activity => {
           return activity.id && activity.startDate !== action.activity.startDate
         })
@@ -35,23 +27,10 @@ export const plannerReducer = (state = [], action) => {
       break;
     case 'DELETE_ACTIVITY':
       return state.filter((activity) => {
-        return (activity.id !== action.activity.id)
+        return activity.id !== action.activity.id
       })
       break;
     case 'HOVER_OVER_ACTIVITY':
-      // let newState = state.filter(activity => {
-      //   return activity.id
-      // })
-      // return [
-      //   ...newState.slice(0, action.index),
-      //   ...[{
-      //     id: '',
-      //     name: '',
-      //     city: '',
-      //     startDate: action.date
-      //   }],
-      //   ...newState.slice(action.index, newState.length)
-      // ]
       let stateWithoutActivitiesWithThatDate = state.filter(activity => {
         return activity.id && activity.startDate !== action.date
       })
@@ -73,19 +52,6 @@ export const plannerReducer = (state = [], action) => {
       ]
       break;
     case 'PLANNERACTIVITY_HOVER_OVER_ACTIVITY':
-      // let newState2 = state.filter(activity => {
-      //   return activity.id && activity.id !== action.activity.id
-      // })
-      // return [
-      //   ...newState2.slice(0, action.index),
-      //   ...[{
-      //     id: '',
-      //     name: '',
-      //     city: '',
-      //     startDate: action.date
-      //   }],
-      //   ...newState2.slice(action.index, newState2.length)
-      // ]
       let stateWithoutPlannerActivitiesWithThatDate = state.filter(activity => {
         return activity.id && activity.startDate !== action.date && activity.id !== action.activity.id
       })
