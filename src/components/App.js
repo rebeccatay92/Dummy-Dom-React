1ed3bce8ce4688ce14fb8be261cc83793031ff9e
Rebecca Tay <rebeccatay92@gmail.com>
2017-11-23 18:05:43 +0800

    testing map component. script moved to head tag. compose fails

--- src/components/App.js
+++ src/components/App.js
@@ -55,7 +55,13 @@ class App extends Component {
             <ItineraryPage />
           )} />
           <Route path='/planner/:itineraryId' component={PlannerPage} />
-          <Route path='/maps' component={ReactPlacesAutocomplete} />
+          <Route path='/maps' component={() => (
+            <ReactPlacesAutocomplete isMarkerShown
+              googleMapURL='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />} />
+          )} />
         </div>
       </Router>
     )
--- src/components/ReactPlacesAutocomplete.js
+++ src/components/ReactPlacesAutocomplete.js
@@ -1,39 +1,39 @@
 import React, {Component} from 'react'
+import { compose, withProps } from 'recompose'
+import { withGoogleMap, withScriptJs, GoogleMap } from 'react-google-maps'
 
 class ReactPlacesAutocomplete extends Component {
   constructor (props) {
     super(props)
-    let timeout
-    this.state = {
-      search: ''
-    }
+    // let timeout
+    // this.state = {
+    //   search: ''
+    // }
   }
 
-  handleChange (e) {
-    this.setState({search: e.target.value})
-  }
+  // handleChange (e) {
+  //   this.setState({search: e.target.value})
+  // }
+  //
+  // customDebounce () {
+  //   var queryStr = this.state.search
+  //   clearTimeout(this.timeout)
+  //   this.timeout = setTimeout(() => {
+  //     this.autocomplete(queryStr)
+  //   }, 250)
+  // }
+  //
+  // autocomplete () {
+  //   var service = new window.google.maps.places.AutocompleteService()
+  //   service.getPlacePredictions({input: this.state.search}, function (predictions, status) {
+  //     console.log(predictions, status)
+  //   })
+  // }
 
-  customDebounce () {
-    var queryStr = this.state.search
-    clearTimeout(this.timeout)
-    this.timeout = setTimeout(() => {
-      this.autocomplete(queryStr)
-    }, 250)
-  }
-
-  autocomplete () {
-    var service = new window.google.maps.places.AutocompleteService()
-    service.getPlacePredictions({input: this.state.search}, function (predictions, status) {
-      console.log(predictions, status)
-    })
-  }
-
-  render() {
+  render () {
     return (
-      <div>
-        <input onChange={(e) => this.handleChange(e)} onKeyDown={() => this.customDebounce()} />
-      </div>
+      <GoogleMap defaultZoom={10} defaultCenter={{ lat: 1.400, lng: 104.000 }} />
     )
   }
 }
-export default ReactPlacesAutocomplete
+export default withGoogleMap(ReactPlacesAutocomplete)
