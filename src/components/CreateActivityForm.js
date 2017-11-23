5b8daefa565fe345247c84116dd5227d5d643f60
Yang Theng <yangtheng92@gmail.com>
2017-11-23 16:15:32 +0800

    styled attachments

--- src/components/CreateActivityForm.js
+++ src/components/CreateActivityForm.js
@@ -235,8 +235,7 @@ class CreateActivityForm extends Component {
     })
   }
 
-  removeUpload (e) {
-    var index = e.target.value
+  removeUpload (index) {
     var objectName = this.state.attachments[index]
     objectName = objectName.replace('/', '%2F')
     var uriBase = 'https://www.googleapis.com/storage/v1/b/domatotest/o/'
@@ -262,6 +261,9 @@ class CreateActivityForm extends Component {
 
       this.setState({attachments: newAttachmentsArr})
       this.setState({fileNames: newFilesArr})
+      this.setState({thumbnail: false})
+      this.setState({thumbnailUrl: null})
+      this.setState({hoveringOver: null})
     })
     .catch(err => {
       console.log(err)
@@ -272,6 +274,7 @@ class CreateActivityForm extends Component {
     var fileName = this.state.attachments[i]
     var offset = `${100 * i}px` //need to check element position
     this.setState({offset: offset})
+    this.setState({hoveringOver: i})
 
     if (fileName.match('.pdf')) {
       var url = 'http://media.idownloadblog.com/wp-content/uploads/2016/04/52ff0e80b07d28b590bbc4b30befde52.png'
@@ -285,6 +288,7 @@ class CreateActivityForm extends Component {
   thumbnailMouseLeave (event) {
     this.setState({thumbnail: false})
     this.setState({thumbnailUrl: null})
+    this.setState({hoveringOver: null})
   }
 
   openPreview (event, i) {
@@ -360,82 +364,85 @@ class CreateActivityForm extends Component {
 
   render () {
     return (
-      <div style={{backgroundColor: '#6D6A7A', position: 'fixed', left: 'calc(50% - 414px)', top: 'calc(50% - 283px)', width: '828px', height: '567px', zIndex: 999, color: 'white', boxShadow: '2px 2px 10px 2px rgba(0, 0, 0, .2)'}}>
-        <div style={{width: '335px', height: '90%', display: 'inline-block', verticalAlign: 'top'}}>
-          <LocationSelection selectLocation={location => this.selectLocation(location)} />
-          <input placeholder='Input Activity' type='text' name='name' value={this.state.name} onChange={(e) => this.handleChange(e, 'name')} autoComplete='off' style={{background: 'inherit', outline: 'none', border: 'none', textAlign: 'center', fontSize: '16px', fontWeight: '300', width: '335px', ':hover': { outline: '0.3px solid white' }}} />
-          {/*
-          <h5>Location: {this.state.googlePlaceData.name}</h5>
-          <h5>Address: {this.state.googlePlaceData.address}</h5> */}
-          <div style={{width: '238px', margin: '45px auto 0 auto', textAlign: 'center', border: '0.3px solid white', height: '131px'}}>
-            <div className='planner-date-picker'>
-              <select key={1} name='startDay' onChange={(e) => this.handleChange(e, 'startDay')} value={this.state.startDay} style={{background: 'inherit', border: 'none', outline: 'none', fontSize: '24px', fontWeight: 100, margin: '10px 5px 10px 0px', ':hover': { outline: '0.3px solid white' }}}>
-                {this.state.dates.map((indiv, i) => {
-                  return <option style={{background: '#6D6A7A'}} value={i + 1} key={i}>Day {i + 1}</option>
-                })}
-              </select>
-              <DatePicker customInput={<PlannerDatePicker />} selected={this.state.startDate} dateFormat={'ddd DD MMM YYYY'} minDate={moment.unix(this.state.dates[0])} maxDate={moment.unix(this.state.dates[this.state.dates.length - 1])} onSelect={(e) => this.handleChange(e, 'startDate')} />
+      <div style={{backgroundColor: 'transparent', position: 'fixed', left: 'calc(50% - 414px)', top: 'calc(50% - 283px)', width: '828px', height: '567px', zIndex: 999, color: 'white'}}>
+        <div style={{boxShadow: '2px 2px 10px 2px rgba(0, 0, 0, .2)', height: '90%'}}>
+          <div style={{backgroundColor: '#6D6A7A', width: '335px', height: '100%', display: 'inline-block', verticalAlign: 'top'}}>
+            <LocationSelection selectLocation={location => this.selectLocation(location)} />
+            <input placeholder='Input Activity' type='text' name='name' value={this.state.name} onChange={(e) => this.handleChange(e, 'name')} autoComplete='off' style={{background: 'inherit', outline: 'none', border: 'none', textAlign: 'center', fontSize: '16px', fontWeight: '300', width: '335px', ':hover': { outline: '0.3px solid white' }}} />
+            {/*
+              <h5>Location: {this.state.googlePlaceData.name}</h5>
+              <h5>Address: {this.state.googlePlaceData.address}</h5> */}
+              <div style={{width: '238px', margin: '45px auto 0 auto', textAlign: 'center', border: '0.3px solid white', height: '131px'}}>
+                <div className='planner-date-picker'>
+                  <select key={12345} name='startDay' onChange={(e) => this.handleChange(e, 'startDay')} value={this.state.startDay} style={{background: 'inherit', border: 'none', outline: 'none', fontSize: '24px', fontWeight: 100, margin: '10px 5px 10px 0px', ':hover': { outline: '0.3px solid white' }}}>
+                    {this.state.dates.map((indiv, i) => {
+                      return <option style={{background: '#6D6A7A'}} value={i + 1} key={i}>Day {i + 1}</option>
+                    })}
+                  </select>
+                  <DatePicker customInput={<PlannerDatePicker />} selected={this.state.startDate} dateFormat={'ddd DD MMM YYYY'} minDate={moment.unix(this.state.dates[0])} maxDate={moment.unix(this.state.dates[this.state.dates.length - 1])} onSelect={(e) => this.handleChange(e, 'startDate')} />
+                </div>
+                <div className='planner-time-picker'>
+                  <input style={{background: 'inherit', fontSize: '16px', outline: 'none', border: 'none', textAlign: 'center'}} type='time' name='startTime' value={this.state.startTime} onChange={(e) => this.handleChange(e, 'startTime')} /> <span>to</span>
+                  <input style={{background: 'inherit', fontSize: '16px', outline: 'none', border: 'none', textAlign: 'center'}} type='time' name='endTime' value={this.state.endTime} onChange={(e) => this.handleChange(e, 'endTime')} />
+                </div>
+                <div className='planner-date-picker'>
+                  <select key={12346} name='endDay' onChange={(e) => this.handleChange(e, 'endDay')} value={this.state.endDay} style={{background: 'inherit', border: 'none', outline: 'none', fontSize: '24px', fontWeight: 100, margin: '10px 5px 10px 0px', ':hover': { outline: '0.3px solid white' }}}>
+                    {this.state.dates.map((indiv, i) => {
+                      if (i + 1 >= this.state.startDay) {
+                        return <option style={{background: '#6D6A7A'}} value={i + 1} key={i}>Day {i + 1}</option>
+                      }
+                    })}
+                  </select>
+                  <DatePicker customInput={<PlannerDatePicker />} selected={this.state.endDate} dateFormat={'ddd DD MMM YYYY'} minDate={this.state.startDate} maxDate={moment.unix(this.state.dates[this.state.dates.length - 1])} onSelect={(e) => this.handleChange(e, 'endDate')} />
+                </div>
+              </div>
             </div>
-            <div className='planner-time-picker'>
-              <input style={{background: 'inherit', fontSize: '16px', outline: 'none', border: 'none', textAlign: 'center'}} type='time' name='startTime' value={this.state.startTime} onChange={(e) => this.handleChange(e, 'startTime')} /> <span>to</span>
-              <input style={{background: 'inherit', fontSize: '16px', outline: 'none', border: 'none', textAlign: 'center'}} type='time' name='endTime' value={this.state.endTime} onChange={(e) => this.handleChange(e, 'endTime')} />
+            <div style={{width: '493px', height: '100%', display: 'inline-block', verticalAlign: 'top', position: 'relative', color: '#3c3a44'}}>
+              <div style={{width: '100%', height: '100%', background: 'white', padding: '65px 2% 2% 77px'}}>
+                <div style={{position: 'absolute', top: '20px', right: '20px', color: '#9FACBC'}}>
+                  <i onClick={() => this.handleSubmit()} className='material-icons' style={{marginRight: '5px', cursor: 'pointer'}}>done</i>
+                  <i onClick={() => this.closeCreateActivity()} className='material-icons' style={{cursor: 'pointer'}}>clear</i>
+                </div>
+                <h4 style={{fontSize: '24px'}}>Booking Details</h4>
+                <label style={{fontSize: '13px', display: 'block', margin: '0', lineHeight: '26px'}}>
+                  Service
+                </label>
+                <input style={{width: '80%'}} type='text' name='bookedThrough' value={this.state.bookedThrough} onChange={(e) => this.handleChange(e, 'bookedThrough')} />
+                <label style={{fontSize: '13px', display: 'block', margin: '0', lineHeight: '26px'}}>
+                  Confirmation Number
+                </label>
+                <input style={{width: '80%'}} type='text' name='bookingConfirmation' value={this.state.bookingConfirmation} onChange={(e) => this.handleChange(e, 'bookingConfirmation')} />
+                <label style={{fontSize: '13px', display: 'block', margin: '0', lineHeight: '26px'}}>
+                  Amount:
+                </label>
+                <select style={{height: '25px', borderRight: '0', background: 'white', width: '20%'}} name='currency' value={this.state.currency} onChange={(e) => this.handleChange(e, 'currency')}>
+                  {this.state.currencyList.map((e, i) => {
+                    return <option key={i}>{e}</option>
+                  })}
+                </select>
+                <input style={{width: '60%'}} type='number' name='cost' value={this.state.cost} onChange={(e) => this.handleChange(e, 'cost')} />
+                <h4 style={{fontSize: '24px'}}>
+                  Additional Notes
+                </h4>
+                <textarea type='text' name='notes' value={this.state.notes} onChange={(e) => this.handleChange(e, 'notes')} style={{width: '200px', height: '100px', display: 'block'}} />
+                <div>
+                  {/* <button onClick={() => this.handleSubmit()}>Create New Activity</button>
+                  <button onClick={() => this.closeCreateActivity()}>Cancel</button> */}
+                </div>
+              </div>
             </div>
-            <div className='planner-date-picker'>
-              <select key={2} name='endDay' onChange={(e) => this.handleChange(e, 'endDay')} value={this.state.endDay} style={{background: 'inherit', border: 'none', outline: 'none', fontSize: '24px', fontWeight: 100, margin: '10px 5px 10px 0px', ':hover': { outline: '0.3px solid white' }}}>
-                {this.state.dates.map((indiv, i) => {
-                  if (i + 1 >= this.state.startDay) {
-                    return <option style={{background: '#6D6A7A'}} value={i + 1} key={i}>Day {i + 1}</option>
-                  }
-                })}
-              </select>
-              <DatePicker customInput={<PlannerDatePicker />} selected={this.state.endDate} dateFormat={'ddd DD MMM YYYY'} minDate={this.state.startDate} maxDate={moment.unix(this.state.dates[this.state.dates.length - 1])} onSelect={(e) => this.handleChange(e, 'endDate')} />
-            </div>
-          </div>
         </div>
-        <div style={{width: '493px', height: '90%', display: 'inline-block', verticalAlign: 'top', position: 'relative', color: '#3c3a44'}}>
-          <div style={{width: '100%', height: '100%', position: 'absolute', background: 'white', padding: '65px 2% 2% 77px'}}>
-            <div style={{position: 'absolute', top: '20px', right: '20px', color: '#9FACBC'}}>
-              <i onClick={() => this.handleSubmit()} className='material-icons' style={{marginRight: '5px', cursor: 'pointer'}}>done</i>
-              <i onClick={() => this.closeCreateActivity()} className='material-icons' style={{cursor: 'pointer'}}>clear</i>
-            </div>
-            <h4 style={{fontSize: '24px'}}>Booking Details</h4>
-            <label style={{fontSize: '13px', display: 'block', margin: '0', lineHeight: '26px'}}>
-              Service
-            </label>
-            <input style={{width: '80%'}} type='text' name='bookedThrough' value={this.state.bookedThrough} onChange={(e) => this.handleChange(e, 'bookedThrough')} />
-            <label style={{fontSize: '13px', display: 'block', margin: '0', lineHeight: '26px'}}>
-              Confirmation Number
-            </label>
-            <input style={{width: '80%'}} type='text' name='bookingConfirmation' value={this.state.bookingConfirmation} onChange={(e) => this.handleChange(e, 'bookingConfirmation')} />
-            <label style={{fontSize: '13px', display: 'block', margin: '0', lineHeight: '26px'}}>
-              Amount:
-            </label>
-            <select style={{height: '25px', borderRight: '0', background: 'white', width: '20%'}} name='currency' value={this.state.currency} onChange={(e) => this.handleChange(e, 'currency')}>
-              {this.state.currencyList.map((e, i) => {
-                return <option key={i}>{e}</option>
-              })}
-            </select>
-            <input style={{width: '60%'}} type='number' name='cost' value={this.state.cost} onChange={(e) => this.handleChange(e, 'cost')} />
-            <h4 style={{fontSize: '24px'}}>
-              Additional Notes
-            </h4>
-            <textarea type='text' name='notes' value={this.state.notes} onChange={(e) => this.handleChange(e, 'notes')} style={{width: '200px', height: '100px', display: 'block'}} />
-            <div>
-              {/* <button onClick={() => this.handleSubmit()}>Create New Activity</button>
-              <button onClick={() => this.closeCreateActivity()}>Cancel</button> */}
-            </div>
-          </div>
-        </div>
-        <div style={{width: '100%', height: '10%', background: 'white'}}>
+        <div style={{minWidth: '20%', background: 'transparent', marginLeft: '20px', display: 'inline-block'}}>
           <div>
             <label style={{display: 'inline-block', color: 'black'}}>
-            Upload
-            <input type='file' name='file' accept='.jpeg, .jpg, .png, .pdf' onChange={(e) => this.handleFileUpload(e)} style={{display: 'none'}} />
+              <i style={{color: '#EDB5BF', margin: '2px 5px 0 0', cursor: 'pointer'}} className='material-icons'>add_circle_outline</i>
+              <input type='file' name='file' accept='.jpeg, .jpg, .png, .pdf' onChange={(e) => this.handleFileUpload(e)} style={{display: 'none'}} />
             </label>
             {this.state.fileNames.map((name, i) => {
-              return <div style={{margin: '0 10px 0 10px', display: 'inline-block', ':hover': {color: 'blue'}}} key={i}>
-                <span onMouseEnter={(event) => this.thumbnailMouseEnter(event, i)} onMouseLeave={(event) => this.thumbnailMouseLeave(event)} onClick={(e) => this.openPreview(e, i)} style={{fontSize: '10px', color: 'black'}}>{name}</span>
-                <button value={i} onClick={(e) => this.removeUpload(e)} style={{fontSize: '10px'}}>X</button>
+              return <div onMouseEnter={(event) => this.thumbnailMouseEnter(event, i)} onMouseLeave={(event) => this.thumbnailMouseLeave(event)} style={{margin: '1px 10px 0 0', verticalAlign: 'top', display: 'inline-block', ':hover': {color: '#EDB5BF'}}} key={i}>
+                <i className='material-icons' style={{color: '#EDB5BF', marginRight: '2px'}}>folder</i>
+                <span onClick={(e) => this.openPreview(e, i)} style={{fontSize: '14px', color: '#EDB5BF', fontWeight: 'bold', cursor: 'pointer', position: 'relative', top: '-6px'}}>{name}</span>
+                {this.state.hoveringOver === i && <i className='material-icons' value={i} onClick={() => this.removeUpload(i)} style={{color: '#EDB5BF'}}>clear</i>}
               </div>
             })}
             {this.state.thumbnail &&
