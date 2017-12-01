88b5912925e23bd5cfbb227b231ea43a2ee4768a
Rebecca Tay <rebeccatay92@gmail.com>
2017-12-01 16:51:49 +0800

    trying selectLocation on HOC instead

--- src/components/location/GoogleMapHOC.js
+++ src/components/location/GoogleMapHOC.js
@@ -24,7 +24,6 @@ const MyMapComponent = compose(
         markers: [],
         infoOpen: false,
         markerIndex: null,
-        testing: null,
         googlePlaceData: {
           placeId: null,
           name: null,
@@ -62,54 +61,50 @@ const MyMapComponent = compose(
             this.setState({markerIndex: null})
           }
         },
-        selectLocation (placeId) {
-          console.log(placeId)
-
-          console.log('button this', this)
-          console.log('refs', refs)
-
-          // this.setState({testing: 'testing'})
-
-          // button scope does hv setState?!!!
-
-          // var request = {placeId: placeId}
-          //
-          // if (refs.map) {
-          //   var service = new window.google.maps.places.PlacesService(refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED)
-          // }
-          //
-          // // bind callback (place, status) scope to lifecycle
-          // service.getDetails(request, (place, status) => {
-          //   if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-          //     console.log('placeDetails', place)
-          //     if (place.address_components) {
-          //       var addressArr = place.address_components
-          //       var countryCode = null
-          //       addressArr.forEach(e => {
-          //         if (e.types.includes('country')) {
-          //           countryCode = e.short_name
-          //         }
-          //       })
-          //     }
-          //
-          //     var openingHours = null
-          //     if (place.opening_hours && place.opening_hours.periods) {
-          //       openingHours = place.opening_hours.periods
-          //     }
-          //
-          //     this.setState({testing: 'hello'})
-          //     // this.setState({googlePlaceData: {
-          //     //   placeId: place.place_id,
-          //     //   countryCode: countryCode,
-          //     //   name: place.name,
-          //     //   address: place.formatted_address,
-          //     //   openingHours: openingHours,
-          //     //   latitude: place.geometry.location.lat(),
-          //     //   longitude: place.geometry.location.lng()
-          //     // }})
-          //   }
-          // })
-        },
+        // selectLocation (placeId) {
+        //   console.log(placeId)
+        //
+        //   console.log('button this', this)
+        //   console.log('refs', refs)
+        //
+        //   // var request = {placeId: placeId}
+        //   //
+        //   // if (refs.map) {
+        //   //   var service = new window.google.maps.places.PlacesService(refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED)
+        //   // }
+        //   //
+        //   // // bind callback (place, status) scope to lifecycle
+        //   // service.getDetails(request, (place, status) => {
+        //   //   if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+        //   //     console.log('placeDetails', place)
+        //   //     if (place.address_components) {
+        //   //       var addressArr = place.address_components
+        //   //       var countryCode = null
+        //   //       addressArr.forEach(e => {
+        //   //         if (e.types.includes('country')) {
+        //   //           countryCode = e.short_name
+        //   //         }
+        //   //       })
+        //   //     }
+        //   //
+        //   //     var openingHours = null
+        //   //     if (place.opening_hours && place.opening_hours.periods) {
+        //   //       openingHours = place.opening_hours.periods
+        //   //     }
+        //   //
+        //   //     this.setState({testing: 'hello'})
+        //   //     // this.setState({googlePlaceData: {
+        //   //     //   placeId: place.place_id,
+        //   //     //   countryCode: countryCode,
+        //   //     //   name: place.name,
+        //   //     //   address: place.formatted_address,
+        //   //     //   openingHours: openingHours,
+        //   //     //   latitude: place.geometry.location.lat(),
+        //   //     //   longitude: place.geometry.location.lng()
+        //   //     // }})
+        //   //   }
+        //   // })
+        // },
         closeInfoWindow: () => {
           console.log('this', this)
           this.setState({infoOpen: false})
@@ -184,7 +179,8 @@ const MyMapComponent = compose(
               <h5>Name: {marker.place.name}</h5>
               <h5>Address: {marker.place.formatted_address}</h5>
               <h5>place_id: {marker.place.place_id}</h5>
-              <button onClick={() => props.selectLocation(marker.place.place_id)}>Select this location</button>
+              {/* <button onClick={() => props.selectLocation(marker.place.place_id)}>Select this location</button> */}
+              <button onClick={() => props.selectLocation(marker.place.place_id)}>Higher Order</button>
             </div>
           </InfoWindow>
       }
@@ -194,11 +190,60 @@ const MyMapComponent = compose(
 )
 
 class CustomMap extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      higherOrder: 'hoc component'
+    }
+  }
+  selectLocation (placeId) {
+    console.log('higher order placeId', placeId)
+    console.log('higher order this', this)
+    console.log('map?', MAP)
 
+    // var service = new window.google.maps.places.PlacesService(MAP.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED)
+    // console.log('service', service)
+
+      // var request = {placeId: placeId}
+
+      // if (refs.map) {
+      //   var service = new window.google.maps.places.PlacesService(refs.map.context.__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED)
+      // }
+      //
+      // // bind callback (place, status) scope to lifecycle
+      // service.getDetails(request, (place, status) => {
+      //   if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+      //     console.log('placeDetails', place)
+      //     if (place.address_components) {
+      //       var addressArr = place.address_components
+      //       var countryCode = null
+      //       addressArr.forEach(e => {
+      //         if (e.types.includes('country')) {
+      //           countryCode = e.short_name
+      //         }
+      //       })
+      //     }
+      //
+      //     var openingHours = null
+      //     if (place.opening_hours && place.opening_hours.periods) {
+      //       openingHours = place.opening_hours.periods
+      //     }
+      //     this.setState({googlePlaceData: {
+      //       placeId: place.place_id,
+      //       countryCode: countryCode,
+      //       name: place.name,
+      //       address: place.formatted_address,
+      //       openingHours: openingHours,
+      //       latitude: place.geometry.location.lat(),
+      //       longitude: place.geometry.location.lng()
+      //     }})
+      //   }
+      // })
+  }
   render () {
     return (
       <div>
-        <MyMapComponent />
+        <MyMapComponent higherOrder={this.state.higherOrder} selectLocation={this.selectLocation} />
       </div>
     )
   }
