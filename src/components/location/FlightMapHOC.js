0f7067850f401773c225a59bf65c54f2f1159925
Rebecca Tay <rebeccatay92@gmail.com>
2017-12-18 22:19:40 +0800

    forced FlightMap infobox to remount on location change by changing key. fitbounds buggy if infobox is longer than 100px

--- src/components/location/FlightMapHOC.js
+++ src/components/location/FlightMapHOC.js
@@ -33,6 +33,7 @@ const FlightMap = compose(
         },
         onMarkerMounted: ref => {
           refs.marker = ref
+          console.log('marker mounted')
         },
         handleMarkerChange: () => {
           var departure = this.props.departureLocation
@@ -56,6 +57,7 @@ const FlightMap = compose(
         },
         onDepartureWindowMounted: ref => {
           refs.departureWindow = ref
+          console.log('departure window mounted')
         },
         onArrivalWindowMounted: ref => {
           refs.arrivalWindow = ref
@@ -69,6 +71,7 @@ const FlightMap = compose(
       })
     },
     componentDidUpdate (prevProps) {
+      // console.log('this', this)
       if ((this.props.departureLocation !== prevProps.departureLocation) || (this.props.arrivalLocation !== prevProps.arrivalLocation)) {
         this.state.handleMarkerChange()
       }
@@ -78,10 +81,9 @@ const FlightMap = compose(
   withGoogleMap
 )((props) =>
   <GoogleMap ref={props.onMapMounted} defaultZoom={2} center={props.center} onBoundsChanged={props.onBoundsChanged} style={{position: 'relative'}} options={{fullscreenControl: false, mapTypeControl: false, streetViewControl: false, zoomControl: false, gestureHandling: 'none'}}>
-
-    {/* {props.departureLocation &&
+    {props.departureLocation &&
       <Marker ref={props.onMarkerMounted} position={{lat: props.departureLocation.latitude, lng: props.departureLocation.longitude}} location={props.departureLocation} clickable={false}>
-        <InfoBox position={new window.google.maps.LatLng(props.departureLocation.latitude, props.departureLocation.longitude)} options={{closeBoxURL: ``, enableEventPropagation: true, pixelOffset: new window.google.maps.Size(-100, -100)}}>
+        <InfoBox key={`${props.departureLocation.latitude}${props.departureLocation.longitude}`} ref={props.onDepartureWindowMounted} position={new window.google.maps.LatLng(props.departureLocation.latitude, props.departureLocation.longitude)} options={{closeBoxURL: ``, enableEventPropagation: true, pixelOffset: new window.google.maps.Size(-100, -100)}}>
           <div style={{backgroundColor: `white`, padding: `10px`, borderRadius: '2px'}}>
             <div style={{ fontSize: `14px`, fontColor: `#08233B` }}>
               {props.departureLocation.name}
@@ -89,8 +91,8 @@ const FlightMap = compose(
           </div>
         </InfoBox>
       </Marker>
-    } */}
-    {props.departureLocation &&
+    }
+    {/* {props.departureLocation &&
       <Marker ref={props.onMarkerMounted} position={{lat: props.departureLocation.latitude, lng: props.departureLocation.longitude}} location={props.departureLocation} onClick={props.toggleDepartureWindow}>
         {props.departureWindow &&
           <InfoWindow ref={props.onDepartureWindowMounted} onCloseClick={props.toggleDepartureWindow}>
@@ -100,7 +102,7 @@ const FlightMap = compose(
           </InfoWindow>
         }
       </Marker>
-    }
+    } */}
     {props.arrivalLocation &&
       <Marker ref={props.onMarkerMounted} position={{lat: props.arrivalLocation.latitude, lng: props.arrivalLocation.longitude}} location={props.arrivalLocation} onClick={props.toggleArrivalWindow}>
         {props.arrivalWindow &&
@@ -114,7 +116,7 @@ const FlightMap = compose(
     }
 
     {(props.departureLocation && props.arrivalLocation) &&
-      <Polyline path={[{lat: props.departureLocation.latitude, lng: props.departureLocation.longitude}, {lat: props.arrivalLocation.latitude, lng: props.arrivalLocation.longitude}]} options={{geodesic: true, icons: [{icon: {path: window.google.maps.SymbolPath.FORWARD_CLOSED_ARROW}, offset: '50%'}]}} />
+      <Polyline path={[{lat: props.departureLocation.latitude, lng: props.departureLocation.longitude}, {lat: props.arrivalLocation.latitude, lng: props.arrivalLocation.longitude}]} options={{geodesic: true, icons: [{icon: {path: 'M22.1,15.1c0,0-1.4-1.3-3-3l0-1.9c0-0.2-0.2-0.4-0.4-0.4l-0.5,0c-0.2,0-0.4,0.2-0.4,0.4l0,0.7c-0.5-0.5-1.1-1.1-1.6-1.6l0-1.5c0-0.2-0.2-0.4-0.4-0.4l-0.4,0c-0.2,0-0.4,0.2-0.4,0.4l0,0.3c-1-0.9-1.8-1.7-2-1.9c-0.3-0.2-0.5-0.3-0.6-0.4l-0.3-3.8c0-0.2-0.3-0.9-1.1-0.9c-0.8,0-1.1,0.8-1.1,0.9L9.7,6.1C9.5,6.2,9.4,6.3,9.2,6.4c-0.3,0.2-1,0.9-2,1.9l0-0.3c0-0.2-0.2-0.4-0.4-0.4l-0.4,0C6.2,7.5,6,7.7,6,7.9l0,1.5c-0.5,0.5-1.1,1-1.6,1.6l0-0.7c0-0.2-0.2-0.4-0.4-0.4l-0.5,0c-0.2,0-0.4,0.2-0.4,0.4l0,1.9c-1.7,1.6-3,3-3,3c0,0.1,0,1.2,0,1.2s0.2,0.4,0.5,0.4s4.6-4.4,7.8-4.7c0.7,0,1.1-0.1,1.4,0l0.3,5.8l-2.5,2.2c0,0-0.2,1.1,0,1.1c0.2,0.1,0.6,0,0.7-0.2c0.1-0.2,0.6-0.2,1.4-0.4c0.2,0,0.4-0.1,0.5-0.2c0.1,0.2,0.2,0.4,0.7,0.4c0.5,0,0.6-0.2,0.7-0.4c0.1,0.1,0.3,0.1,0.5,0.2c0.8,0.2,1.3,0.2,1.4,0.4c0.1,0.2,0.6,0.3,0.7,0.2c0.2-0.1,0-1.1,0-1.1l-2.5-2.2l0.3-5.7c0.3-0.3,0.7-0.1,1.6-0.1c3.3,0.3,7.6,4.7,7.8,4.7c0.3,0,0.5-0.4,0.5-0.4S22,15.3,22.1,15.1z', fillColor: 'white', fillOpacity: 1, anchor: new window.google.maps.Point(11, 11), scale: 2, strokeWeight: 2}, offset: '50%'}]}} />
     }
   </GoogleMap>
 )
