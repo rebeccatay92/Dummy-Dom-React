e5bb5d5e88c3f036b209e53331f146d660a59b81
Rebecca Tay <rebeccatay92@gmail.com>
2018-01-03 14:43:41 +0800

    stash before merge upstream 3rd jan

--- src/helpers/newEventTimelineValidation.js
+++ src/helpers/newEventTimelineValidation.js
@@ -2,28 +2,28 @@
 
 function newEventTimelineValidation (eventsArr, model, newEvent) {
   // construct timeline arr. lodging gets point timings for checkin and checkout
-  console.log('eventsArr', eventsArr)
+  // console.log('eventsArr', eventsArr)
   if (model === 'Activity' || model === 'Food') {
     var isSameDay = (newEvent.startDay === newEvent.endDay)
     if (isSameDay) {
-      // take out dayEvents
       var dayEvents = eventsArr.filter(e => {
         return e.day === newEvent.startDay
       })
       console.log('dayEvents', dayEvents)
-      // var timelineArr = []
-      // dayEvents.forEach(event => {
-      //   if (event.type !== 'Activity' && event.type !== 'Food') {
-      //     timelineArr.push({
-      //       type: event.type,
-      //       start: event.start,
-      //       time: event.time
-      //     })
-      //   }
-      // })
       console.log('startTime', newEvent.startTime, 'endTime', newEvent.endTime)
-      // if start unix is in between anything clash, if end unix is in between anything => clash
-      // if both dont cut between start and end, find the row startUnix displaces, end unix must also be before displacedRow
+      var displacedRow = dayEvents.find(e => {
+        return (e.time >= newEvent.startTime)
+      })
+
+      if (!displacedRow) {
+        // if displacedRow is null (last event), check start:true is not before it (excluding lodging)
+        var displacedIndex = dayEvents.indexOf(displacedRow)
+        var previousRow = dayEvents[displacedIndex - 1]
+        var isStartingRow = (typeof (previousRow.start) === 'boolean' && previousRow.start && previousRow.type !== 'Lodging')
+        // if isStartingRow is true, validation error.
+      }
+      // if displacedRow exists, check if it is an ending row of type start:false (not lodging)
+      // if displacedRow exists but not an ending row, check end time is also before displaced time, else overlap
     } else { // if different start and end day
       // must be last on day1 (only one row)
       // end time must be earlier than first event on next day
